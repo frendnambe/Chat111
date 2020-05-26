94a08a43212686b7455ede97148f9e2453ab5d4b
frendnambe <49234020+frendnambe@users.noreply.github.com>
2020-05-26 15:13:10 +0300

    Добавьте файлы проекта.

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChatClient.ServiceChat;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window ,IServiseChatCallback
+    
+    {
+        bool isConnected = false;
+        ServiseChatClient client;
+        int ID;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+ 
+        }
+
+
+        void ConnectUser()
+        {
+            client = new ServiseChatClient(new System.ServiceModel.InstanceContext(this));
+            ID = client.Connect(tbUserName.Text);
+            tbUserName.IsEnabled = false;
+            bConnDiscon.Content = "Disconnect";
+            isConnected = true;
+        }
+
+        void DisonnectUser()
+        {
+            if (!isConnected)
+            {
+                client.Disconnect(ID);
+                client = null;
+                tbUserName.IsEnabled = true;
+                bConnDiscon.Content = "Connect";
+                isConnected = false;
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isConnected)
+            {
+                DisonnectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }    
+        }
+        public void MsgCallback(string msg)
+        {
+            lbChat.Items.Add(msg);
+            lbChat.ScrollIntoView(lbChat.Items[lbChat.Items.Count - 1]);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisonnectUser();
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (client!=null)
+                {
+                    client.SendMsg(tbMessage.Text, ID);
+                    tbMessage.Text = string.Empty;
+                }
+                
+            }
+        }
+    }
+}
